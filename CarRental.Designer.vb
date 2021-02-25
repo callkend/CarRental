7f6ff5979676998ad07020005b8bd53a5f0f05f9
callkend <77285430+callkend@users.noreply.github.com>
2021-02-25 09:03:20 -0700

    Started Adding Code

--- CarRental.Designer.vb
+++ CarRental.Designer.vb
@@ -42,64 +42,78 @@ Partial Class CarRental
         Me.MilesRadioButton = New System.Windows.Forms.RadioButton()
         Me.KilometersRadioButton = New System.Windows.Forms.RadioButton()
         Me.DiscountsGroupBox = New System.Windows.Forms.GroupBox()
-        Me.AAAMemberDiscountCheckBox = New System.Windows.Forms.CheckBox()
         Me.SeniorCitzenDiscountCheckBox = New System.Windows.Forms.CheckBox()
+        Me.AAAMemberDiscountCheckBox = New System.Windows.Forms.CheckBox()
+        Me.DistanceDrivenInMilesLabel = New System.Windows.Forms.Label()
+        Me.MileageChargeLabel = New System.Windows.Forms.Label()
+        Me.DayChargeLabel = New System.Windows.Forms.Label()
+        Me.MinusDiscountLabel = New System.Windows.Forms.Label()
+        Me.YouOweLabel = New System.Windows.Forms.Label()
+        Me.DistanceDrivenInMilesTextBox = New System.Windows.Forms.TextBox()
+        Me.MileageChargeTextBox = New System.Windows.Forms.TextBox()
+        Me.DayChargeTextBox = New System.Windows.Forms.TextBox()
+        Me.MinusDiscountTextBox = New System.Windows.Forms.TextBox()
+        Me.YouOweTextBox = New System.Windows.Forms.TextBox()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SummaryButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
         Me.MilesOrKilometersGroupBox.SuspendLayout()
         Me.DiscountsGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'CustomerNameTextBox
         '
-        Me.CustomerNameTextBox.Location = New System.Drawing.Point(12, 29)
+        Me.CustomerNameTextBox.Location = New System.Drawing.Point(210, 12)
         Me.CustomerNameTextBox.Name = "CustomerNameTextBox"
         Me.CustomerNameTextBox.Size = New System.Drawing.Size(100, 22)
         Me.CustomerNameTextBox.TabIndex = 0
         '
         'AddressTextBox
         '
-        Me.AddressTextBox.Location = New System.Drawing.Point(12, 74)
+        Me.AddressTextBox.Location = New System.Drawing.Point(210, 54)
         Me.AddressTextBox.Name = "AddressTextBox"
         Me.AddressTextBox.Size = New System.Drawing.Size(100, 22)
         Me.AddressTextBox.TabIndex = 1
         '
         'CityTextBox
         '
-        Me.CityTextBox.Location = New System.Drawing.Point(12, 119)
+        Me.CityTextBox.Location = New System.Drawing.Point(210, 99)
         Me.CityTextBox.Name = "CityTextBox"
         Me.CityTextBox.Size = New System.Drawing.Size(100, 22)
         Me.CityTextBox.TabIndex = 2
         '
         'StateTextBox
         '
-        Me.StateTextBox.Location = New System.Drawing.Point(12, 164)
+        Me.StateTextBox.Location = New System.Drawing.Point(210, 144)
         Me.StateTextBox.Name = "StateTextBox"
         Me.StateTextBox.Size = New System.Drawing.Size(100, 22)
         Me.StateTextBox.TabIndex = 3
         '
         'ZipCodeTextBox
         '
-        Me.ZipCodeTextBox.Location = New System.Drawing.Point(12, 209)
+        Me.ZipCodeTextBox.Location = New System.Drawing.Point(210, 189)
         Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
         Me.ZipCodeTextBox.Size = New System.Drawing.Size(100, 22)
         Me.ZipCodeTextBox.TabIndex = 4
         '
         'BeginningOdometerReadingTextBox
         '
-        Me.BeginningOdometerReadingTextBox.Location = New System.Drawing.Point(12, 254)
+        Me.BeginningOdometerReadingTextBox.Location = New System.Drawing.Point(210, 234)
         Me.BeginningOdometerReadingTextBox.Name = "BeginningOdometerReadingTextBox"
         Me.BeginningOdometerReadingTextBox.Size = New System.Drawing.Size(100, 22)
         Me.BeginningOdometerReadingTextBox.TabIndex = 5
         '
         'EndingOdometerReadingTextBox
         '
-        Me.EndingOdometerReadingTextBox.Location = New System.Drawing.Point(12, 299)
+        Me.EndingOdometerReadingTextBox.Location = New System.Drawing.Point(210, 279)
         Me.EndingOdometerReadingTextBox.Name = "EndingOdometerReadingTextBox"
         Me.EndingOdometerReadingTextBox.Size = New System.Drawing.Size(100, 22)
         Me.EndingOdometerReadingTextBox.TabIndex = 6
         '
         'NumberOfDaysTextBox
         '
-        Me.NumberOfDaysTextBox.Location = New System.Drawing.Point(12, 344)
+        Me.NumberOfDaysTextBox.Location = New System.Drawing.Point(210, 324)
         Me.NumberOfDaysTextBox.Name = "NumberOfDaysTextBox"
         Me.NumberOfDaysTextBox.Size = New System.Drawing.Size(100, 22)
         Me.NumberOfDaysTextBox.TabIndex = 7
@@ -190,6 +204,7 @@ Partial Class CarRental
         'MilesRadioButton
         '
         Me.MilesRadioButton.AutoSize = True
+        Me.MilesRadioButton.Checked = True
         Me.MilesRadioButton.Location = New System.Drawing.Point(6, 21)
         Me.MilesRadioButton.Name = "MilesRadioButton"
         Me.MilesRadioButton.Size = New System.Drawing.Size(61, 21)
@@ -220,16 +235,6 @@ Partial Class CarRental
         Me.DiscountsGroupBox.TabStop = False
         Me.DiscountsGroupBox.Text = "Enter Any Discounts"
         '
-        'AAAMemberDiscountCheckBox
-        '
-        Me.AAAMemberDiscountCheckBox.AutoSize = True
-        Me.AAAMemberDiscountCheckBox.Location = New System.Drawing.Point(6, 21)
-        Me.AAAMemberDiscountCheckBox.Name = "AAAMemberDiscountCheckBox"
-        Me.AAAMemberDiscountCheckBox.Size = New System.Drawing.Size(171, 21)
-        Me.AAAMemberDiscountCheckBox.TabIndex = 0
-        Me.AAAMemberDiscountCheckBox.Text = "AAA Member Discount"
-        Me.AAAMemberDiscountCheckBox.UseVisualStyleBackColor = True
-        '
         'SeniorCitzenDiscountCheckBox
         '
         Me.SeniorCitzenDiscountCheckBox.AutoSize = True
@@ -240,11 +245,157 @@ Partial Class CarRental
         Me.SeniorCitzenDiscountCheckBox.Text = "Senior Citzen Discount"
         Me.SeniorCitzenDiscountCheckBox.UseVisualStyleBackColor = True
         '
-        'Form1
+        'AAAMemberDiscountCheckBox
+        '
+        Me.AAAMemberDiscountCheckBox.AutoSize = True
+        Me.AAAMemberDiscountCheckBox.Location = New System.Drawing.Point(6, 21)
+        Me.AAAMemberDiscountCheckBox.Name = "AAAMemberDiscountCheckBox"
+        Me.AAAMemberDiscountCheckBox.Size = New System.Drawing.Size(171, 21)
+        Me.AAAMemberDiscountCheckBox.TabIndex = 0
+        Me.AAAMemberDiscountCheckBox.Text = "AAA Member Discount"
+        Me.AAAMemberDiscountCheckBox.UseVisualStyleBackColor = True
+        '
+        'DistanceDrivenInMilesLabel
+        '
+        Me.DistanceDrivenInMilesLabel.AutoSize = True
+        Me.DistanceDrivenInMilesLabel.Location = New System.Drawing.Point(555, 12)
+        Me.DistanceDrivenInMilesLabel.Name = "DistanceDrivenInMilesLabel"
+        Me.DistanceDrivenInMilesLabel.Size = New System.Drawing.Size(159, 17)
+        Me.DistanceDrivenInMilesLabel.TabIndex = 20
+        Me.DistanceDrivenInMilesLabel.Text = "Distance Driven In Miles"
+        '
+        'MileageChargeLabel
+        '
+        Me.MileageChargeLabel.AutoSize = True
+        Me.MileageChargeLabel.Location = New System.Drawing.Point(555, 54)
+        Me.MileageChargeLabel.Name = "MileageChargeLabel"
+        Me.MileageChargeLabel.Size = New System.Drawing.Size(107, 17)
+        Me.MileageChargeLabel.TabIndex = 21
+        Me.MileageChargeLabel.Text = "Mileage Charge"
+        '
+        'DayChargeLabel
+        '
+        Me.DayChargeLabel.AutoSize = True
+        Me.DayChargeLabel.Location = New System.Drawing.Point(555, 99)
+        Me.DayChargeLabel.Name = "DayChargeLabel"
+        Me.DayChargeLabel.Size = New System.Drawing.Size(83, 17)
+        Me.DayChargeLabel.TabIndex = 22
+        Me.DayChargeLabel.Text = "Day Charge"
+        '
+        'MinusDiscountLabel
+        '
+        Me.MinusDiscountLabel.AutoSize = True
+        Me.MinusDiscountLabel.Location = New System.Drawing.Point(555, 144)
+        Me.MinusDiscountLabel.Name = "MinusDiscountLabel"
+        Me.MinusDiscountLabel.Size = New System.Drawing.Size(104, 17)
+        Me.MinusDiscountLabel.TabIndex = 23
+        Me.MinusDiscountLabel.Text = "Minus Discount"
+        '
+        'YouOweLabel
+        '
+        Me.YouOweLabel.AutoSize = True
+        Me.YouOweLabel.Location = New System.Drawing.Point(555, 189)
+        Me.YouOweLabel.Name = "YouOweLabel"
+        Me.YouOweLabel.Size = New System.Drawing.Size(65, 17)
+        Me.YouOweLabel.TabIndex = 24
+        Me.YouOweLabel.Text = "You Owe"
+        '
+        'DistanceDrivenInMilesTextBox
+        '
+        Me.DistanceDrivenInMilesTextBox.Enabled = False
+        Me.DistanceDrivenInMilesTextBox.Location = New System.Drawing.Point(734, 12)
+        Me.DistanceDrivenInMilesTextBox.Name = "DistanceDrivenInMilesTextBox"
+        Me.DistanceDrivenInMilesTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.DistanceDrivenInMilesTextBox.TabIndex = 25
+        '
+        'MileageChargeTextBox
+        '
+        Me.MileageChargeTextBox.Enabled = False
+        Me.MileageChargeTextBox.Location = New System.Drawing.Point(734, 51)
+        Me.MileageChargeTextBox.Name = "MileageChargeTextBox"
+        Me.MileageChargeTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.MileageChargeTextBox.TabIndex = 26
+        '
+        'DayChargeTextBox
+        '
+        Me.DayChargeTextBox.Enabled = False
+        Me.DayChargeTextBox.Location = New System.Drawing.Point(734, 94)
+        Me.DayChargeTextBox.Name = "DayChargeTextBox"
+        Me.DayChargeTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.DayChargeTextBox.TabIndex = 27
+        '
+        'MinusDiscountTextBox
+        '
+        Me.MinusDiscountTextBox.Enabled = False
+        Me.MinusDiscountTextBox.Location = New System.Drawing.Point(734, 141)
+        Me.MinusDiscountTextBox.Name = "MinusDiscountTextBox"
+        Me.MinusDiscountTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.MinusDiscountTextBox.TabIndex = 28
+        '
+        'YouOweTextBox
+        '
+        Me.YouOweTextBox.Enabled = False
+        Me.YouOweTextBox.Location = New System.Drawing.Point(734, 186)
+        Me.YouOweTextBox.Name = "YouOweTextBox"
+        Me.YouOweTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.YouOweTextBox.TabIndex = 29
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(174, 520)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(75, 32)
+        Me.CalculateButton.TabIndex = 30
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(368, 520)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(75, 32)
+        Me.ClearButton.TabIndex = 31
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SummaryButton
+        '
+        Me.SummaryButton.Enabled = False
+        Me.SummaryButton.Location = New System.Drawing.Point(547, 520)
+        Me.SummaryButton.Name = "SummaryButton"
+        Me.SummaryButton.Size = New System.Drawing.Size(75, 32)
+        Me.SummaryButton.TabIndex = 32
+        Me.SummaryButton.Text = "Summary"
+        Me.SummaryButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(719, 520)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 32)
+        Me.ExitButton.TabIndex = 33
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'CarRental
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(952, 577)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SummaryButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.YouOweTextBox)
+        Me.Controls.Add(Me.MinusDiscountTextBox)
+        Me.Controls.Add(Me.DayChargeTextBox)
+        Me.Controls.Add(Me.MileageChargeTextBox)
+        Me.Controls.Add(Me.DistanceDrivenInMilesTextBox)
+        Me.Controls.Add(Me.YouOweLabel)
+        Me.Controls.Add(Me.MinusDiscountLabel)
+        Me.Controls.Add(Me.DayChargeLabel)
+        Me.Controls.Add(Me.MileageChargeLabel)
+        Me.Controls.Add(Me.DistanceDrivenInMilesLabel)
         Me.Controls.Add(Me.DiscountsGroupBox)
         Me.Controls.Add(Me.MilesOrKilometersGroupBox)
         Me.Controls.Add(Me.NumberOfDaysLabel)
@@ -263,7 +414,7 @@ Partial Class CarRental
         Me.Controls.Add(Me.CityTextBox)
         Me.Controls.Add(Me.AddressTextBox)
         Me.Controls.Add(Me.CustomerNameTextBox)
-        Me.Name = "Form1"
+        Me.Name = "CarRental"
         Me.Text = "Form1"
         Me.MilesOrKilometersGroupBox.ResumeLayout(False)
         Me.MilesOrKilometersGroupBox.PerformLayout()
@@ -296,4 +447,18 @@ Partial Class CarRental
     Friend WithEvents DiscountsGroupBox As GroupBox
     Friend WithEvents SeniorCitzenDiscountCheckBox As CheckBox
     Friend WithEvents AAAMemberDiscountCheckBox As CheckBox
+    Friend WithEvents DistanceDrivenInMilesLabel As Label
+    Friend WithEvents MileageChargeLabel As Label
+    Friend WithEvents DayChargeLabel As Label
+    Friend WithEvents MinusDiscountLabel As Label
+    Friend WithEvents YouOweLabel As Label
+    Friend WithEvents DistanceDrivenInMilesTextBox As TextBox
+    Friend WithEvents MileageChargeTextBox As TextBox
+    Friend WithEvents DayChargeTextBox As TextBox
+    Friend WithEvents MinusDiscountTextBox As TextBox
+    Friend WithEvents YouOweTextBox As TextBox
+    Friend WithEvents CalculateButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SummaryButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- CarRental.vb
+++ CarRental.vb
@@ -1,3 +1,127 @@
-﻿Public Class CarRental
+﻿Option Strict On
+Option Explicit On
+
+Public Class CarRental
+    Dim problemMessage As String = ""
+    Dim problem As Boolean = False
+    Dim beginningOdometer As Integer
+    Dim endingOdometer As Integer
+    Dim numberOfDays As Integer
+    Dim numberOfMilesDriven As Integer
+    Dim mileageCharge As Integer
+
+
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        If Validater(CustomerNameTextBox.Text, False) = "is empty" Then
+            problem = True
+            problemMessage += "Name text box is empty "
+            CustomerNameTextBox.Select()
+        End If
+
+        If Validater(AddressTextBox.Text, False) = "is empty" Then
+            If problem = False Then
+                AddressTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "Address text box is empty "
+
+        End If
+
+        If Validater(CityTextBox.Text, False) = "is empty" Then
+            If problem = False Then
+                CityTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "City text box is empty"
+        End If
+
+        If Validater(StateTextBox.Text, False) = "is empty" Then
+            If problem = False Then
+                StateTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "State text box is empty "
+        End If
+
+        If Validater(ZipCodeTextBox.Text, True) = "must contain number" Then
+            If problem = False Then
+                ZipCodeTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "Zip Code text box is empty "
+        End If
+
+        If Validater(BeginningOdometerReadingTextBox.Text, True) = "must contain number" Then
+            If problem = False Then
+                BeginningOdometerReadingTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "Beginning Odometer must be a number "
+        Else
+            beginningOdometer = CInt(BeginningOdometerReadingTextBox.Text)
+        End If
+
+        If Validater(EndingOdometerReadingTextBox.Text, True) = "must contain number" Then
+            If problem = False Then
+                EndingOdometerReadingTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "Ending Odometer must be a number "
+        Else
+            endingOdometer = CInt(EndingOdometerReadingTextBox.Text)
+        End If
+
+        If Validater(NumberOfDaysTextBox.Text, True) = "must contain number" Then
+            If problem = False Then
+                NumberOfDaysTextBox.Select()
+            End If
+            problem = True
+            problemMessage += "Number of Days must be a number "
+        Else
+            numberOfDays = CInt(NumberOfDaysTextBox.Text)
+        End If
+
+        MessageBox.Show(problemMessage)
+
+        If MilesRadioButton.Checked = True Then
+            numberOfMilesDriven = endingOdometer - beginningOdometer
+            DistanceDrivenInMilesTextBox.Text = CStr(numberOfMilesDriven)
+
+        End If
+
+    End Sub
+
+    Function Validater(textBox As String, isInteger As Boolean) As String
+        Dim holder As Integer
+        Dim response As String
+        Select Case isInteger
+            Case True
+                Try
+                    holder = CInt(textBox)
+                    response = textBox
+                Catch ex As Exception
+                    response = "must contain number"
+                End Try
+            Case False
+                If textBox = "" Then
+                    response = "is empty"
+                Else
+                    response = textBox
+                End If
+        End Select
+        Return response
+    End Function
+
+
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Dim userInput As Double
+
+        userInput = MsgBox("Do you want to exit", CType(vbYesNo + vbCritical + vbDefaultButton2, MsgBoxStyle), "Exit")
+        If userInput = vbYes Then
+            Me.Close()
+        End If
+    End Sub
 
 End Class
